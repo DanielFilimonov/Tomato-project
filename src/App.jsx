55d6e7ab8561768a4b8b3ff02ec807df0906f79c
DanielFilimonov <marseanen85@gmail.com>
2025-11-07 14:55:26 +0300

    фикс багов

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,28 @@ import LogginPopup from "./components/LogginPopup/LogginPopup";
 import Navbar from "./components/Navbar/Navbar";
 import Cart from "./pages/Cart/Cart";
 import Home from "./pages/Home/Home";
-
-import { useEffect, useState } from "react";
-import { Routes, Route, useLocation } from "react-router-dom";
+import ScrollToTop from "./components/utils/ScrollToTop";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
 	const [showLogin, setShowLogin] = useState(false);
 	// const [activeSection, setActiveSection] = useState("menu");
 
-  //   const { hash } = useLocation();
+	//   const { hash } = useLocation();
 
-	
 	// useEffect(() => {
 	// 	const sections = document.querySelectorAll(".section");
 
 	// 	const observer = new IntersectionObserver(
 	// 		(entries) => {
 	// 			entries.forEach((entry) => {
-  //                   if (entry.isIntersecting) {
-  //                       const newSection = entry.target.id;
-  //                       setActiveSection(newSection);
-                        
-  //                       const newHash = `#${newSection}`;
-                        
+	//                   if (entry.isIntersecting) {
+	//                       const newSection = entry.target.id;
+	//                       setActiveSection(newSection);
+
+	//                       const newHash = `#${newSection}`;
+
 	// 					if (window.location.hash !== newHash) {
 	// 						window.history.replaceState(null, "", newHash);
 	// 					}
@@ -43,11 +42,11 @@ const App = () => {
 
 	return (
 		<>
+			<ScrollToTop />
 			{showLogin ? <LogginPopup setShowLogin={setShowLogin} /> : <></>}
+
+			<Navbar setShowLogin={setShowLogin} />
 			<div className="app">
-				<Navbar
-					setShowLogin={setShowLogin}
-				/>
 				<Routes>
 					<Route path="/" element={<Home />} />
 					<Route path="/cart" element={<Cart />} />
@@ -59,4 +58,3 @@ const App = () => {
 };
 
 export default App;
-
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,31 @@
 import { useSelector } from "react-redux";
-import { useEffect } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import "./Navbar.css";
 
 const Navbar = ({ setShowLogin }) => {
+	const [isScrolled, setIsScrolled] = useState(false);
+
 	const cart = useSelector((state) => state.cart);
-	const {activeSection} = useSelector((state) => state.activeSection);
+	const { activeSection } = useSelector((state) => state.activeSection);
 
 	const location = useLocation();
+	const navigate = useNavigate();
 	const hash = location.hash;
 	const isAtHomePage = location.pathname === "/";
+
+	
+	const handleClick = () => {
+		if (isAtHomePage) {
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+		} else {
+			navigate("/");
+		}
+	};
 
 	useEffect(() => {
 		if (hash) {
@@ -21,56 +36,87 @@ const Navbar = ({ setShowLogin }) => {
 		}
 	}, [hash]);
 
+	useEffect(() => {
+		const handleScroll = () => setIsScrolled(window.scrollY > 80);
+		window.addEventListener("scroll", handleScroll);
+		return () => window.removeEventListener("scroll", handleScroll);
+	}, []);
+
+	useEffect(() => {
+		if (isAtHomePage && activeSection) {
+			const newHash = `#${activeSection}`;
+			if (window.location.hash !== newHash) {
+				window.history.replaceState(null, "", newHash);
+			}
+		}
+	}, [activeSection, isAtHomePage]);
+
 	const divDotClass = Object.keys(cart).length > 0 ? "dot" : "";
 
 	return (
-		<div className="navbar">
-			<Link to="/">
-				<img src={assets.logo} alt="" className="logo" />{" "}
-			</Link>
-			<ul className="navbar-menu">
-				<li
-					className={`${
-						isAtHomePage && activeSection === "home" ? "active" : ""
-					}`}
-				>
-					<Link to="/#home">home</Link>
-				</li>
-				<li
-					className={`${
-						isAtHomePage && activeSection === "menu" ? "active" : ""
-					}`}
-				>
-					<Link to="/#menu">menu</Link>
-				</li>
-				<li
-					className={`${
-						isAtHomePage && activeSection === "mobile-app"
-							? "active"
-							: ""
-					}`}
-				>
-					<Link to="/#mobile-app">mobile-app</Link>
-				</li>
-				<li
-					className={`${
-						isAtHomePage && activeSection === "contact-us"
-							? "active"
-							: ""
-					}`}
-				>
-					<Link to="/#contact-us">contact us</Link>
-				</li>
-			</ul>
-			<div className="navbar-right">
-				<img src={assets.search_icon} alt="" />
-				<div className="navbar-search-icon">
-					<Link to="/cart">
-						<img src={assets.basket_icon} alt="" />
+		<div className={`navbar-wrapper ${isScrolled ? "shadow" : ""}`}>
+			<div className="app">
+				<div className="navbar">
+					<Link to="/">
+						<img
+							onClick={handleClick}
+							src={assets.logo}
+							alt=""
+							className="logo"
+						/>{" "}
 					</Link>
-					<div className={divDotClass}></div>
+					<ul className="navbar-menu">
+						<li
+							onClick={handleClick}
+							className={`${
+								isAtHomePage && activeSection === "home"
+									? "active"
+									: ""
+							}`}
+						>
+							<Link to="/">home</Link>
+						</li>
+						<li
+							className={`${
+								isAtHomePage && activeSection === "menu"
+									? "active"
+									: ""
+							}`}
+						>
+							<Link to="/#menu">menu</Link>
+						</li>
+						<li
+							className={`${
+								isAtHomePage && activeSection === "mobile-app"
+									? "active"
+									: ""
+							}`}
+						>
+							<Link to="/#mobile-app">mobile-app</Link>
+						</li>
+						<li
+							className={`${
+								isAtHomePage && activeSection === "contact-us"
+									? "active"
+									: ""
+							}`}
+						>
+							<Link to="/#contact-us">contact us</Link>
+						</li>
+					</ul>
+					<div className="navbar-right">
+						<img src={assets.search_icon} alt="" />
+						<div className="navbar-search-icon">
+							<Link to="/cart">
+								<img src={assets.basket_icon} alt="" />
+							</Link>
+							<div className={divDotClass}></div>
+						</div>
+						<button onClick={() => setShowLogin(true)}>
+							sign in
+						</button>
+					</div>
 				</div>
-				<button onClick={() => setShowLogin(true)}>sign in</button>
 			</div>
 		</div>
 	);
